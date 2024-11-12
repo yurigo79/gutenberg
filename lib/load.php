074dc52e6b3ac5362392d309e79ebc63eb884a57
Ramon <ramonjd@users.noreply.github.com>
2024-11-13 09:45:18 +1100

    Media: add `x-wav` mime type for wav files in Firefox (#66850)
    Adding x-wav support because Firefox uses that identifier
    Check if wav does not exist in case some plugins or themes have already filtered it out.
    
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: Imran92 <imranh920@git.wordpress.org>

--- lib/load.php
+++ lib/load.php
@@ -119,6 +119,7 @@ require __DIR__ . '/compat/wordpress-6.7/post-formats.php';
 // WordPress 6.8 compat.
 require __DIR__ . '/compat/wordpress-6.8/preload.php';
 require __DIR__ . '/compat/wordpress-6.8/blocks.php';
+require __DIR__ . '/compat/wordpress-6.8/functions.php';
 
 // Experimental features.
 require __DIR__ . '/experimental/block-editor-settings-mobile.php';
