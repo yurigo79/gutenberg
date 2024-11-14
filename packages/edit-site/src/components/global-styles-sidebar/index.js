2e5ba3b94ac5cf17be5b947b449a42a71334295a
Ella <4710635+ellatrix@users.noreply.github.com>
2024-11-14 13:14:37 +0100

    Site Editor > Styles: open styles inspector when clicking preview canvas (#66996)
    Co-authored-by: ellatrix <ellatrix@git.wordpress.org>
    Co-authored-by: youknowriad <youknowriad@git.wordpress.org>
    Co-authored-by: jasmussen <joen@git.wordpress.org>

--- packages/edit-site/src/components/global-styles-sidebar/index.js
+++ packages/edit-site/src/components/global-styles-sidebar/index.js
@@ -5,7 +5,7 @@ import { FlexItem, Flex, Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { styles, seen, backup } from '@wordpress/icons';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { useEffect } from '@wordpress/element';
+import { useEffect, useRef } from '@wordpress/element';
 import { store as preferencesStore } from '@wordpress/preferences';
 import {
 	store as editorStore,
@@ -29,7 +29,7 @@ const { useLocation } = unlock( routerPrivateApis );
 
 export default function GlobalStylesSidebar() {
 	const { params } = useLocation();
-	const { canvas = 'view' } = params;
+	const { canvas = 'view', path } = params;
 	const {
 		shouldClearCanvasContainerView,
 		isStyleBookOpened,
@@ -127,6 +127,20 @@ export default function GlobalStylesSidebar() {
 			isStyleBookOpened ? undefined : 'style-book'
 		);
 	};
+
+	const { getActiveComplementaryArea } = useSelect( interfaceStore );
+	const { enableComplementaryArea } = useDispatch( interfaceStore );
+	const previousActiveAreaRef = useRef( null );
+
+	useEffect( () => {
+		if ( path === '/wp_global_styles' && canvas === 'edit' ) {
+			previousActiveAreaRef.current =
+				getActiveComplementaryArea( 'core' );
+			enableComplementaryArea( 'core', 'edit-site/global-styles' );
+		} else if ( previousActiveAreaRef.current ) {
+			enableComplementaryArea( 'core', previousActiveAreaRef.current );
+		}
+	}, [ path, enableComplementaryArea, canvas, getActiveComplementaryArea ] );
 
 	return (
 		<DefaultSidebar
