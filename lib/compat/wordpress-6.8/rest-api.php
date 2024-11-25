a6a04bebfb96f47df6c1f238c0fe63233d6dd3f5
Bernie Reiter <96308+ockham@users.noreply.github.com>
2024-11-25 11:20:51 +0100

    Terms: Respect order specified by register_taxonomy() (#67154)
    It is possible to supply a default set of `args` to `register_taxonomy()` which will be used when querying a list of terms -- for example, `orderby` in order to specify how the resulting list of terms should be sorted. This commit makes it so that the list of terms returned by the Terms REST API controller respects that order.
    
    Co-authored-by: ockham <bernhard-reiter@git.wordpress.org>
    Co-authored-by: jsnajdr <jsnajdr@git.wordpress.org>

--- lib/compat/wordpress-6.8/rest-api.php
+++ lib/compat/wordpress-6.8/rest-api.php
@@ -20,3 +20,32 @@ if ( ! function_exists( 'gutenberg_add_post_type_rendering_mode' ) ) {
 	}
 }
 add_action( 'rest_api_init', 'gutenberg_add_post_type_rendering_mode' );
+
+// When querying terms for a given taxonomy in the REST API, respect the default
+// query arguments set for that taxonomy upon registration.
+function gutenberg_respect_taxonomy_default_args_in_rest_api( $args ) {
+	// If a `post` argument is provided, the Terms controller will use
+	// `wp_get_object_terms`, which respects the default query arguments,
+	// so we don't need to do anything.
+	if ( ! empty( $args['post'] ) ) {
+		return $args;
+	}
+
+	$t = get_taxonomy( $args['taxonomy'] );
+	if ( isset( $t->args ) && is_array( $t->args ) ) {
+		$args = array_merge( $args, $t->args );
+	}
+	return $args;
+}
+add_action(
+	'registered_taxonomy',
+	function ( $taxonomy ) {
+		add_filter( "rest_{$taxonomy}_query", 'gutenberg_respect_taxonomy_default_args_in_rest_api' );
+	}
+);
+add_action(
+	'unregistered_taxonomy',
+	function ( $taxonomy ) {
+		remove_filter( "rest_{$taxonomy}_query", 'gutenberg_respect_taxonomy_default_args_in_rest_api' );
+	}
+);
