bb56ce8350cb0ddef9df3bcb75b24fa5e81a9ab3
Ramon <ramonjd@users.noreply.github.com>
2024-12-05 06:14:07 +1100

    Edit site: remove empty preview border and redirect to editor in global styles navigation (#67548)
    Co-authored-by: ramonjd <ramonopoly@git.wordpress.org>
    Co-authored-by: t-hamano <wildworks@git.wordpress.org>
    Co-authored-by: talldan <talldanwp@git.wordpress.org>

--- packages/edit-site/src/components/site-editor-routes/styles.js
+++ packages/edit-site/src/components/site-editor-routes/styles.js
@@ -1,9 +1,28 @@
 /**
+ * WordPress dependencies
+ */
+import { privateApis as routerPrivateApis } from '@wordpress/router';
+
+/**
  * Internal dependencies
  */
 import Editor from '../editor';
+import { unlock } from '../../lock-unlock';
 import SidebarNavigationScreenGlobalStyles from '../sidebar-navigation-screen-global-styles';
 import GlobalStylesUIWrapper from '../sidebar-global-styles-wrapper';
+
+const { useLocation } = unlock( routerPrivateApis );
+
+function MobileGlobalStylesUI() {
+	const { query = {} } = useLocation();
+	const { canvas } = query;
+
+	if ( canvas === 'edit' ) {
+		return <Editor />;
+	}
+
+	return <GlobalStylesUIWrapper />;
+}
 
 export const stylesRoute = {
 	name: 'styles',
@@ -12,7 +31,7 @@ export const stylesRoute = {
 		content: <GlobalStylesUIWrapper />,
 		sidebar: <SidebarNavigationScreenGlobalStyles backPath="/" />,
 		preview: <Editor />,
-		mobile: <GlobalStylesUIWrapper />,
+		mobile: <MobileGlobalStylesUI />,
 	},
 	widths: {
 		content: 380,
