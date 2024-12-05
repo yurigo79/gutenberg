e8a7463ac0664c860487d30bfb04a435d63c8e4a
George Mamadashvili <georgemamadashvili@gmail.com>
2024-12-05 08:47:22 +0400

    Block Editor: Reduce the 'isZoomOut' selector calls in the block toolbar (#67594)
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: talldan <talldanwp@git.wordpress.org>

--- packages/block-editor/src/components/block-toolbar/index.js
+++ packages/block-editor/src/components/block-toolbar/index.js
@@ -124,6 +124,8 @@ export function PrivateBlockToolbar( {
 			( id ) => getTemplateLock( id ) === 'contentOnly'
 		);
 
+		const _isZoomOut = isZoomOut();
+
 		return {
 			blockClientId: selectedBlockClientId,
 			blockClientIds: selectedBlockClientIds,
@@ -132,7 +134,7 @@ export function PrivateBlockToolbar( {
 			shouldShowVisualToolbar: isValid && isVisual,
 			toolbarKey: `${ selectedBlockClientId }${ parentClientId }`,
 			showParentSelector:
-				! isZoomOut() &&
+				! _isZoomOut &&
 				parentBlockType &&
 				getBlockEditingMode( parentClientId ) !== 'disabled' &&
 				hasBlockSupport(
@@ -144,11 +146,11 @@ export function PrivateBlockToolbar( {
 			isUsingBindings: _isUsingBindings,
 			hasParentPattern: _hasParentPattern,
 			hasContentOnlyLocking: _hasTemplateLock,
-			showShuffleButton: isZoomOut(),
-			showSlots: ! isZoomOut(),
-			showGroupButtons: ! isZoomOut(),
-			showLockButtons: ! isZoomOut(),
-			showSwitchSectionStyleButton: isZoomOut(),
+			showShuffleButton: _isZoomOut,
+			showSlots: ! _isZoomOut,
+			showGroupButtons: ! _isZoomOut,
+			showLockButtons: ! _isZoomOut,
+			showSwitchSectionStyleButton: _isZoomOut,
 			hasFixedToolbar: getSettings().hasFixedToolbar,
 			isNavigationMode: _isNavigationMode(),
 		};
