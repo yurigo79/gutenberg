4db31201fb8e62d9393f096a79e82a3b21aea074
Lena Morita <lena@jaguchi.com>
2024-12-13 04:14:37 +0900

    FontFamilyControl: Deprecate 36px default size (#67853)
    Co-authored-by: mirka <0mirka00@git.wordpress.org>
    Co-authored-by: tyxla <tyxla@git.wordpress.org>

--- packages/block-editor/src/components/font-family/index.js
+++ packages/block-editor/src/components/font-family/index.js
@@ -58,6 +58,20 @@ export default function FontFamilyControl( {
 		);
 	}
 
+	if (
+		! __next40pxDefaultSize &&
+		( props.size === undefined || props.size === 'default' )
+	) {
+		deprecated(
+			`36px default size for wp.blockEditor.__experimentalFontFamilyControl`,
+			{
+				since: '6.8',
+				version: '7.1',
+				hint: 'Set the `__next40pxDefaultSize` prop to true to start opting into the new default size, which will become the default in a future version.',
+			}
+		);
+	}
+
 	return (
 		<CustomSelectControl
 			__next40pxDefaultSize={ __next40pxDefaultSize }
