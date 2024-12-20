854b84db032768e3d7932fe006a09b8824fa9404
Rishit Gupta <rishitgupta2013@gmail.com>
2024-12-20 14:36:38 +0530

    Fix: Inserter category tabs: avoid unnecessary aria-label (#68160)
    Co-authored-by: Rishit30G <rishit30g@git.wordpress.org>
    Co-authored-by: Mamaduka <mamaduka@git.wordpress.org>
    Co-authored-by: afercia <afercia@git.wordpress.org>

--- packages/block-editor/src/components/inserter/category-tabs/index.js
+++ packages/block-editor/src/components/inserter/category-tabs/index.js
@@ -64,7 +64,6 @@ function CategoryTabs( {
 					<Tabs.Tab
 						key={ category.name }
 						tabId={ category.name }
-						aria-label={ category.label }
 						aria-current={
 							category === selectedCategory ? 'true' : undefined
 						}
